a8e82768225237b594baf1e476bacc2f2fa2f440
mtanner161 <m.tanner161@gmail.com>
2023-02-21 20:24:36 -0600

    KING-15 adding gas allocation and last 30 day pull

--- sendProductionToComboCurve.py
+++ sendProductionToComboCurve.py
@@ -20,9 +20,9 @@ from combocurve_api_v1.pagination import get_next_page_url
 load_dotenv()  # load enviroment variables
 
 # connect to service account
-service_account = ServiceAccount.from_file(os.getenv("API_SEC_CODE_LIVE"))
+service_account = ServiceAccount.from_file(os.getenv("API_SEC_CODE"))
 # set API Key from enviroment variable
-api_key = os.getenv("API_KEY_PASS_LIVE")
+api_key = os.getenv("API_KEY_PASS")
 # specific Python ComboCurve authentication
 combocurve_auth = ComboCurveAuth(service_account, api_key)
 
@@ -32,13 +32,16 @@ print("Authentication Worked")
 totalAssetProduction = pd.read_csv(r".\kingops\data\totalAssetsProduction.csv")
 
 totalAssetProduction.rename(
-    columns={"Oil Volume": "oil", "Date": "date", "Gas Volume": "gas", "Client": "customText0"}, inplace=True)
+    columns={"Oil Volume": "oil", "Date": "date", "Gas Volume": "gas", "Client": "customText0", "Water Volume": "water"}, inplace=True)
 
 totalAssetProduction.replace({"South Texas": "KOSOU", "East Texas": "KOEAS", "Gulf Coast": "KOGCT",
                              "Midcon": "KOAND", "Permian Basin": "KOPRM", "Colorado": "WELOP", "Scurry": "KOPRM"}, inplace=True)
 
 totalAssetProduction.to_json(
-    r".\kingops\data\totalAssetsProduction.json", orient="records")
+    r".\kingoperating\data\totalAssetsProduction.json", orient="records")
+
+totalAssetProductionJson = totalAssetProduction.to_json(orient="records")
+cleanTotalAssetProduction = json.loads(totalAssetProductionJson)
 
 
 print("yay")
