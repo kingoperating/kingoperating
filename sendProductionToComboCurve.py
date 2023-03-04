9b533b84a4d5024b5ef7a32e5d256b522c64fd12
mtanner161 <m.tanner161@gmail.com>
2023-03-03 20:37:36 -0600

    KING-37 all test volumes sent to combocurve

--- sendProductionToComboCurve.py
+++ sendProductionToComboCurve.py
@@ -20,9 +20,9 @@ from combocurve_api_v1.pagination import get_next_page_url
 load_dotenv()  # load enviroment variables
 
 # connect to service account
-service_account = ServiceAccount.from_file(os.getenv("API_SEC_CODE_LIVE"))
+service_account = ServiceAccount.from_file(os.getenv("API_SEC_CODE"))
 # set API Key from enviroment variable
-api_key = os.getenv("API_KEY_PASS_LIVE")
+api_key = os.getenv("API_KEY_PASS")
 # specific Python ComboCurve authentication
 combocurve_auth = ComboCurveAuth(service_account, api_key)
 
@@ -31,6 +31,8 @@ print("Authentication Worked")
 # bring in total asset production
 totalAssetProduction = pd.read_csv(
     r".\kingoperating\data\comboCurveAllocatedProduction.csv")
+
+totalAssetProduction = totalAssetProduction.iloc[80001:]
 
 totalAssetProduction = totalAssetProduction.drop(
     ["Oil Sold Volume", "Well Accounting Name", "Client"], axis=1)
@@ -48,6 +50,8 @@ totalAssetProduction.to_json(
 totalAssetProductionJson = totalAssetProduction.to_json(orient="records")
 cleanTotalAssetProduction = json.loads(totalAssetProductionJson)
 
+print(len(cleanTotalAssetProduction))
+
 keltonTestData = [{
     "date": "2021-05-01",
     "chosenID": "42357337470000",
@@ -62,20 +66,11 @@ projectId = "64021df74295aa0012e4e3ea"
 url = "https://api.combocurve.com/v1/daily-productions"
 auth_headers = combocurve_auth.get_auth_headers()
 
-response = requests.put(url, headers=auth_headers, json=keltonTestData)
+response = requests.put(url, headers=auth_headers,
+                        json=cleanTotalAssetProduction)
 
 responseCode = response.status_code  # sets response code to the current state
 
-# parse as json string
-results = response.json()
-# setting to length of results
-numEntries = len(results)
-
-if responseCode == 200:
-    print("Status Code is 200")
-    print(str(numEntries) + " entries read")
-else:
-    print("The Status Code: " + str(response.status_code))
-
+print(responseCode)
 
 print("yay")
