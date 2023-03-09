33e6295d93bc1a1ded8c8363739c790f5414c9d6
mtanner161 <m.tanner161@gmail.com>
2023-03-09 13:10:49 -0600

    updating repo

--- afeTotal.py
+++ afeTotal.py
@@ -16,7 +16,7 @@ import numpy as np
 from openpyxl import Workbook
 
 # Set Well Name:
-nameOfWell = "millerrancha501mh"
+nameOfWell = "kinga199cv1h"
 
 # Load in all files needed
 pathOfDailyReport = '.\\kingoperating\\data\\afe' + \
