97132316a5d9c8a375e727238718fbe7e79d89d8
mtanner161 <m.tanner161@gmail.com>
2023-03-14 12:19:26 -0500

    combine send production to CC, allocation and daily production into single run

---
+++ masterProductionRun.py
@@ -0,0 +1,8 @@
+### MASTER PRODUCTION RUN ###
+
+# Runs the three scripts that make up the production run
+import productionFinalGreasebook  # ALWAYS SET TO FALSE
+import productionAllocationProd  # ALWAYS SET TO FALSE
+import sendProductionToComboCurve  # ALWAYS SET TO FALSE
+
+print("MASTER PRODUCTION DONE")
