3eef039cd110e9a13ab38aa4c61a071457418022
mtanner161 <m.tanner161@gmail.com>
2023-06-03 12:16:06 -0500

    pivot working - data loaded through Jan 2023

--- joyn.py
+++ joyn.py
@@ -5,6 +5,7 @@ import re
 import pandas as pd
 import os
 from dotenv import load_dotenv
+import numpy as np
 
 load_dotenv()
 
@@ -107,18 +108,19 @@ def getName(apiNumber2):
 idToken = getIdToken()  # get idToken from authJoyn function
 
 # set correct URL for Reading Data API JOYN - use idToken as header for authorization
-url = "https://api-fdg.joyn.ai/admin/api/ReadingData?isCustom=true&entityids=15408&fromdate=2023-04-01&todate=2023-06-03&pagesize=1000&pagenumber="
+urlBase = "https://api-fdg.joyn.ai/admin/api/ReadingData?isCustom=true&entityids=15408&fromdate=2023-01-01&todate=2023-06-03&pagesize=1000&pagenumber="
 
 pageNumber = 1  # set page number to 1
 nextPage = True
 totalResults = []  # create empty list to store results
 
 while nextPage == True:  # loop through all pages of data
-    url = url + str(pageNumber)  # add page number to url
+    url = urlBase + str(pageNumber)
     # makes the request to the API
     response = requests.request("GET", url, headers={"Authorization": idToken})
     if response.status_code != 200:
         print(response.status_code)
+        print("yay")
 
     print("Length of Response: " + str(len(response.json())))
 
@@ -128,14 +130,19 @@ while nextPage == True:  # loop through all pages of data
     if len(resultsReadingType) == 0:
         # triggers while loop to end when no more data is returned and length of response is 0
         nextPage = False
+
     pageNumber = pageNumber + 1  # increment page number by 1 for pagination
-    url = url[:-1]  # removes last character from url to reset for next page
 
 readingVolume = 0
 
 headers = ["AssetId", "Name", "ReadingVolume",
            "NetworkName", "Date", "Product", "Disposition"]
-totalAssetProduction = pd.DataFrame(columns=headers)
+rawTotalAssetProduction = pd.DataFrame(columns=headers)
+
+headersFinal = ["API Number", "Well Name", "Date",
+                "Oil Volume", "Gas Volume", "Water Volume"]
+finalTotalAssetProduction = pd.DataFrame(columns=headersFinal)
+
 
 for i in range(0, len(totalResults)):
     for j in range(0, len(totalResults[i])):
@@ -166,17 +173,69 @@ for i in range(0, len(totalResults)):
         row = [apiNumber, wellName, readingVolume, networkName,
                dateBetter, newProduct, disposition]
         # append row to dataframe
-        totalAssetProduction.loc[len(totalAssetProduction)] = row
+        rawTotalAssetProduction.loc[len(rawTotalAssetProduction)] = row
 
-totalAssetProduction["Date"] = pd.to_datetime(
-    totalAssetProduction["Date"])
+rawTotalAssetProduction["Date"] = pd.to_datetime(
+    rawTotalAssetProduction["Date"])
 
-totalAssetProductionSorted = totalAssetProduction.sort_values(by=[
+rawTotalAssetProductionSorted = rawTotalAssetProduction.sort_values(by=[
     "Date"])
 
+dailyRawProduction = np.zeros([200, 3])
+dailyRawAssetId = []
+dailyRawWellName = []
+dailyRawDate = []
+priorDate = -999
+
+counter = 0
+lastIndex = 0
+
+for i in range(0, len(rawTotalAssetProductionSorted)):
+    row = rawTotalAssetProductionSorted.iloc[i]
+    apiNumberPivot = row["AssetId"]
+    readingVolumePivot = row["ReadingVolume"]
+    productTypePivot = row["Product"]
+    datePivot = row["Date"]
+    wellNamePivot = getName(apiNumberPivot)
+
+    if datePivot != priorDate and priorDate != -999:
+        for j in range(0, counter):
+            row = [dailyRawAssetId[j], dailyRawWellName[j], dailyRawDate[j],
+                   dailyRawProduction[j][0], dailyRawProduction[j][1], dailyRawProduction[j][2]]
+            finalTotalAssetProduction.loc[lastIndex + j] = row
+
+        dailyRawProduction = np.zeros([200, 3])
+        dailyRawAssetId = []
+        dailyRawWellName = []
+        dailyRawDate = []
+        counter = 0
+        lastIndex = lastIndex + j + 1
+
+    if apiNumberPivot in dailyRawAssetId:
+        index = dailyRawAssetId.index(apiNumberPivot)
+        if productTypePivot == "Oil":
+            dailyRawProduction[index][0] = readingVolumePivot
+        elif productTypePivot == "Gas":
+            dailyRawProduction[index][1] = readingVolumePivot
+        elif productTypePivot == "Water":
+            dailyRawProduction[index][2] = readingVolumePivot
+
+    else:
+        dailyRawAssetId.append(apiNumberPivot)
+        dailyRawWellName.append(wellNamePivot)
+        dailyRawDate.append(datePivot)
+        if productTypePivot == "Oil":
+            dailyRawProduction[counter][0] = readingVolumePivot
+        elif productTypePivot == "Gas":
+            dailyRawProduction[counter][1] = readingVolumePivot
+        elif productTypePivot == "Water":
+            dailyRawProduction[counter][2] = readingVolumePivot
+        counter = counter + 1
+
+    priorDate = datePivot
 
 # export dataframe to csv
-totalAssetProductionSorted.to_csv(
+finalTotalAssetProduction.to_csv(
     r"C:\Users\mtanner\OneDrive - King Operating\Documents 1\code\kingoperating\data\totalAssetProductionJoyn.csv", index=False)
 
 print("yay")
